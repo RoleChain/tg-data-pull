5ea922ddf41286db7ea0c959e1992f111eed5d0b
Harsh48 <harsh.techilia@gmail.com>
2024-07-23 21:46:37 +0530

    fix: issues

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const prompt = require('prompt');
 const { t1 } = require('@mtproto/core');
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors());
+
+// Alternatively, you can specify options to customize the behavior
+const corsOptions = {
+  origin: '*', // This will allow all origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+
+app.use(cors(corsOptions));
 
 // Fetch user details
 async function getUser() {
